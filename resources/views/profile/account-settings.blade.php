f5adc97f1b6823d777c0db5771fe2f177d06fe64
Kavi <kavib2682004@gmail.com>
2025-06-26 13:46:32 +0530

    refactor: Enhance layout and styling of account settings page for improved user experience

--- resources/views/profile/account-settings.blade.php
+++ resources/views/profile/account-settings.blade.php
@@ -4,20 +4,32 @@
     </x-slot>
 
     <div class="sm:ml-64">
-        <div class="w-full max-w-4xl mx-auto">
-            <x-bread-crumb-navigation />
+        <div class="w-full mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white p-6 rounded-2xl shadow m-4 flex flex-col"
+            style="height: 88vh;">
+            <!-- Breadcrumb -->
+            <div class="flex justify-between items-center mb-4">
+                <x-bread-crumb-navigation />
+            </div>
+
+            <hr class="p-2 border-t border-gray-400">
 
             <!-- Update Password Form -->
-            <div class="p-8 mb-6 bg-white border border-gray-200 rounded-lg shadow-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+            <div class="flex flex-col gap-4">
+                <h2 class="text-lg font-semibold text-gray-800">Update Password :</h2>
+                <div class="p-4 sm:p-8 bg-white border border-gray-200 rounded-lg shadow-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.update-password-form')
+                    </div>
                 </div>
             </div>
 
             <!-- Delete User Form -->
-            <div class="p-8 bg-white border border-gray-200 rounded-lg shadow-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+            <div class="flex flex-col gap-4 mt-6">
+                <h2 class="text-lg font-semibold text-gray-800">Delete Account :</h2>
+                <div class="p-4 sm:p-8 bg-white border border-gray-200 rounded-lg shadow-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
             </div>
         </div>
