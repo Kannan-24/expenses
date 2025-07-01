8df2da2b26008bfc9e6bd9cffd781d064cebc2c7
Kannan <kannanmuruganandham1@gmail.com>
2025-07-01 07:46:07 +0530

    fix: Adjust sidebar logout form positioning for improved layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -268,7 +268,7 @@
                 </li>
             </ul>
 
-            <div class="w-full lg:mt-auto">
+            <div class="w-full absolute bottom-0 left-0">
                 <form id="sidebar-logout-form" action="{{ route('logout') }}" method="POST">
                     @csrf
                     <button type="submit"
