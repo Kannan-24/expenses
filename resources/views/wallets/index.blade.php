1e8c3eee6c4719c7b2e856c2dff7d282662c649e
Hariharan K <haripsp2004@gmail.com>
2025-07-08 09:08:38 +0530

    fix: Update transaction and wallet view links with icons for better UX

--- resources/views/wallets/index.blade.php
+++ resources/views/wallets/index.blade.php
@@ -338,9 +338,9 @@
                                     <a href="{{ route('wallets.show', $wallet->id) }}" 
                                        class="inline-flex items-center text-yellow-600 dark:text-yellow-400 text-sm font-medium">
                                         <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                                </svg>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                                        </svg>
                                         View
                                     </a>
                                     <a href="{{ route('wallets.edit', $wallet->id) }}" 
